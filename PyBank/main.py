8c453ec04f1d772f586f35e7d9194779057916dc
justindooley <40034984+justindooley@users.noreply.github.com>
2018-10-09 18:33:55 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,9 @@ import csv
 budget_csvpath = os.path.join("Resources", "budget_data.csv")
 
 data = []
+revenue = []
+rev_change = []
+date = []
 
 with open(budget_csvpath) as budget_csv:
 
@@ -18,11 +21,6 @@ with open(budget_csvpath) as budget_csv:
         data.append({
             csv_header[0]: row[0], csv_header[1]: int(row[1])
         })
-        
-    # Create new lists for higher maths
-    revenue = []
-    rev_change = []
-    date = []
    
     #This fills separate revenue and date lists.
         date.append(row[0])
@@ -37,24 +35,27 @@ with open(budget_csvpath) as budget_csv:
     for data1 in data:
         total += data1['Profit/Losses']
     print(f"Total: ${total}") 
-    
+
+    avg_rev_change = 0
+    max_rev_change = 0
+    min_rev_change = 0
     #in this loop I found average revenue change, max revenue change, and min revenue change. 
     for i in range(1,len(revenue)):
         rev_change.append(revenue[i] - revenue[i-1])   
         avg_rev_change = sum(rev_change)/len(rev_change)
         max_rev_change = max(rev_change)
         min_rev_change = min(rev_change)
-
+        avg_rev_change = round(avg_rev_change,2)
         max_rev_change_date = str(date[rev_change.index(max(rev_change))])
         min_rev_change_date = str(date[rev_change.index(min(rev_change))])
 
 
-    print("Avereage Change: $", (avg_rev_change))
-    print("Greatest Increase in Profits:", max_rev_change_date,"($", max_rev_change,")")
-    print("Greatest Decrease in Profits:", min_rev_change_date,"($", min_rev_change,")")
+    print(f"Avereage Change: $", (avg_rev_change))
+    print(f"Greatest Increase in Profits:", max_rev_change_date,"($", max_rev_change,")")
+    print(f"Greatest Decrease in Profits:", min_rev_change_date,"($", min_rev_change,")")
     
-    #with open("Financial Results.txt", "w") as text_file:
-    #print(f'Financial Analysis\n-------------------------\nTotal Months:' + month_count + '\nAverage  Change:'+ avg_rev_change +'\nGreatest Increase in Profits:' + max_rev_change_date + '($' + max_rev_change + ')' + '\nGreatest Decrease in Profits:' + min_rev_change_date + '($' + min_rev_change + ')', file = text_file)
+    with open("Financial Results.txt", "w") as text_file:
+    print(f'Financial Analysis\n-------------------------\nTotal Months:' + month_count + '\nAverage  Change:'+ avg_rev_change +'\nGreatest Increase in Profits:' + max_rev_change_date + '($' + max_rev_change + ')' + '\nGreatest Decrease in Profits:' + min_rev_change_date + '($' + min_rev_change + ')', file = text_file)
     
 
     
