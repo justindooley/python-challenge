0df1991a03584a1e8cc052db2a2a070eba6bfd71
justindooley <40034984+justindooley@users.noreply.github.com>
2018-10-09 08:20:15 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,22 +4,48 @@ import csv
 
 budget_csvpath = os.path.join("Resources", "budget_data.csv")
 
+data = []
 
 with open(budget_csvpath) as budget_csv:
 
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(budget_csv, delimiter=',')
-    next(csvreader)
-    data = list(budget_csv)
+    csv_header = next(csvreader)
+
+    # Fill data with csv rows and headers
+    for row in csvreader:
+        data.append({
+            csv_header[0]: row[0], csv_header[1]: int(row[1])
+        })
+    print("Financial Analysis")   
+    print("----------------------------")
     month_count = (len(data))
+    print(f"Total Months: {month_count}")   
+
+    total = 0
+    for data1 in data:
+        total += data1['Profit/Losses']
+    print(f"Total: ${total}") 
+
+    average = 0
+
+    print(data)
+
+    # greatest = 0
+    # for data3 in data:
+    #     greatest >= data3['Profit/Losses']
+    # print(f'The Greatest Increase in Profits: {greatest}')
+
+    # data = list(budget_csv)
+    # csv_header = next(csvreader)
     # data = data.remove("\n")
     # data = str.data.replace('\n','')
     # print(data.stript("\n")))
-    print("Total Months: " + str(month_count))
     # print data.replace('\n','')
-    print strip("\n")
-    # for row in data:
+    # print(data(remove("\n"))
+    # print strip("\n")
+    # for row in csvreader:
     #     row = row.split(",")
     #     date , profit = row
         # data = data.strip('\n')
